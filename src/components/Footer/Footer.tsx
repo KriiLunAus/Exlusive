d268be5cfb85730ba91dc0de545e25c48b01bd56
Krii <kriilunaus16@gmail.com>
2025-03-14 13:17:39 +0200

    added fonts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,30 +5,38 @@ export default function Footer() {
     <div className={css.linksWrapper}>
             <div>
                 <h3 className={css.linksHeader}>Exclusive</h3>
-                <p>Subscribe</p>
-                <p>Get 10% off your first order</p>
-                <input type="text" />
+                <div className={css.links}>
+                    <p>Subscribe</p>
+                    <p>Get 10% off your first order</p>
+                    <input type="text" />
+                </div>
         </div>
             <div>
                 <h3 className={css.linksHeader}>Support</h3>
-                <p>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</p>
-                <p>exclusive@gmail.com</p>
-                <p>+88015-88888-9999</p>
+                <div className={css.links}>
+                    <p>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</p>
+                    <p>exclusive@gmail.com</p>
+                    <p>+88015-88888-9999</p>
+                </div>
         </div>
             <div>
                 <h3 className={css.linksHeader}>Account</h3>
-                <p>My Account</p>
-                <p>Login / Register</p>
-                <p>Cart</p>
-                <p>Wishlist</p>
-                <p>Shop</p>
+                <div className={css.links}>
+                    <p>My Account</p>
+                    <p>Login / Register</p>
+                    <p>Cart</p>
+                    <p>Wishlist</p>
+                    <p>Shop</p>
+                </div>
         </div>
             <div>
                 <h3 className={css.linksHeader}>Quick Link</h3>
-                <p>Privacy Policy</p>
-                <p>Terms Of Use</p>
-                <p>FAQ</p>
-                <p>Contact</p>
+                <div className={css.links}>
+                    <p>Privacy Policy</p>
+                    <p>Terms Of Use</p>
+                    <p>FAQ</p>
+                    <p>Contact</p>
+                </div>
             </div>
             <div>
                 <h3 className={css.linksHeader}>Download</h3>
